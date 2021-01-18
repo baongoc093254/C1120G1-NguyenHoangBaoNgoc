a80df7845662e9f7a740bb76ecad5ffaf84a8c2e
ty nguyen <tynguyen@MacBook-Pro-cua-ty.local>
2021-01-18 08:30:59 +0700

    update 18/1

--- Module 2/src/bai10_stack_queue/baitap/ReverseArray.java
+++ Module 2/src/bai10_stack_queue/baitap/ReverseArray.java
@@ -1,27 +1,28 @@
 package bai10_stack_queue.baitap;
 
+import java.util.Arrays;
 import java.util.Collections;
+import java.util.Scanner;
 import java.util.Stack;
 
 public class ReverseArray {
-    public static void main(String[] args) {
+    public static void reverseArr (int[] array) {
         Stack<Integer> stackNum = new Stack<>();
-        stackNum.add(1);
-        stackNum.add(2);
-        stackNum.add(3);
-        stackNum.add(4);
-        stackNum.add(5);
-        stackNum.add(6);
-        stackNum.add(7);
-        System.out.println("Pre-reverse:");
-        for (Integer number: stackNum) {
-            System.out.println(number);
+        System.out.println("Pre-Reverse");
+        for (int i : array) {
+            stackNum.push(i);
         }
+        System.out.println(stackNum);
         System.out.println("Reverse");
-        
-        Stack<Integer> stackNum2 = new Stack<>();
-
-
+        for (int j = 0; j < array.length ; j++) {
+            array[j] = stackNum.pop();
+        }
+        System.out.println(Arrays.toString(array));
     }
 
+    public static void main(String[] args) {
+        int[] arrayList = {1,2,3,4,5};
+        reverseArr(arrayList);
+    }
 }
+
