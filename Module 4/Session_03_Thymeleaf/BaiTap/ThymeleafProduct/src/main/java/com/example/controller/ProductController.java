a55d92c800cb124443ebb326725cb51dbb844532
baongoc093254 <tynguyen1554@gmail.com>
2021-04-04 01:09:12 +0700

    update modal

--- Module 4/Session_03_Thymeleaf/BaiTap/ThymeleafProduct/src/main/java/com/example/controller/ProductController.java
+++ Module 4/Session_03_Thymeleaf/BaiTap/ThymeleafProduct/src/main/java/com/example/controller/ProductController.java
@@ -6,10 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.stereotype.Repository;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.util.ArrayList;
@@ -50,23 +47,25 @@ public class ProductController {
     @PostMapping("/edit")
     public String updateProduct(@ModelAttribute Product product, RedirectAttributes redirectAttributes) {
         productService.update(product.getId(),product);
-        redirectAttributes.addFlashAttribute("message", "Edit infor product !");
+        redirectAttributes.addFlashAttribute("message", "Edit information product !");
         return "redirect:/";
     }
 
-    @GetMapping("/product/{id}/delete")
-    public String getDeleteProduct(@PathVariable int id, Model model) {
-        model.addAttribute("product", productService.findById(id));
-        return "delete";
-
-    }
-
-    @PostMapping("/delete")
-    public String deleteProduct(@ModelAttribute Product product, RedirectAttributes redirectAttributes) {
-        productService.remove(product.getId());
-        redirectAttributes.addFlashAttribute("message", "Delete Product !");
+    @GetMapping("/product/delete")
+    public String getDeleteProduct(@RequestParam int id) {
+        productService.remove(id);
         return "redirect:/";
+//        model.addAttribute("product", productService.findById(id));
+//        return "delete";
+
     }
+
+//    @PostMapping("/delete")
+//    public String deleteProduct(@ModelAttribute Product product, RedirectAttributes redirectAttributes) {
+//        productService.remove(product.getId());
+//        redirectAttributes.addFlashAttribute("message", "Delete Product !");
+//        return "redirect:/";
+//    }
 
     @GetMapping("/product/{id}/detail")
     public String getDetailProduct(@PathVariable int id, Model model) {
